a20176cbbb1441208d3390471098902791a77da2
Benjamin Cronin <bcronin720@gmail.com>
2017-05-09 15:28:24 -0700

    debugging

--- src/DK-BHTanks/GameData/BlackHoleTanks/Plugins/BHStorageCfg.cs
+++ src/DK-BHTanks/GameData/BlackHoleTanks/Plugins/BHStorageCfg.cs
@@ -94,7 +94,7 @@ namespace BHTKSP
 
         static double Req(string res)
         {
-            return Part.RequestResource(res, 0);//Fix needed to make equal to BHECCost...
+            return Part.RequestResource(res, 0.0f);//Fix needed to make equal to BHECCost...
             Debug.Log("BHT Requesting EC");
         }
 
