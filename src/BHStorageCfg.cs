ae7e3483cbb7ad44165e967d07e26930d588d324
Benjamin Cronin <bcronin720@gmail.com>
2017-05-14 17:42:03 -0700

    Added fuel removing functionality

--- src/BHStorageCfg.cs
+++ src/BHStorageCfg.cs
@@ -58,7 +58,6 @@ namespace BHTKSP
         public void Enable()
         {
             BlackHoleEnabled = true;
-            
         }
 
         [KSPEvent(guiActive = false, guiName = "Deactivate Black Hole", active = true)]
@@ -126,7 +125,7 @@ namespace BHTKSP
                 fuel1MaxAmount = GetMaxResourceAmount(Fuel1Name);
                 fuel1MaxAmount = fuel1LastAmount;
                 Debug.Log("BHT Scene is flight, BHT has gotten fuel1 values");
-
+                Remove(Fuel1Name);
                 Debug.Log("BHT Fuel1 amount set to 0.0, BH off");
                 if (FuelTypes == 2)
                 {
@@ -134,7 +133,7 @@ namespace BHTKSP
                     fuel2MaxAmount = GetMaxResourceAmount(Fuel2Name);
                     fuel2MaxAmount = fuel2LastAmount;
                     Debug.Log("BHT has gotten fuel2 values");
-                    
+                    Remove(Fuel2Name);
                     Debug.Log("BHT Fuel2 amount set to 0.0, BH off");
                 }
                 else
