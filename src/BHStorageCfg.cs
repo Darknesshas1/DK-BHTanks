c59aefdcaa7d3cc7f98f9a10d69e79402c41902f
Benjamin Cronin <bcronin720@gmail.com>
2017-05-15 16:49:16 -0700

    debugging

--- src/BHStorageCfg.cs
+++ src/BHStorageCfg.cs
@@ -124,7 +124,7 @@ namespace BHTKSP
                 fuel1MaxAmount = GetMaxResourceAmount(Fuel1Name);
                 fuel1MaxAmount = fuel1LastAmount;
                 Debug.Log("BHT Scene is flight, BHT has gotten fuel1 values");
-                public bool Remove(Fuel1Name); //Removing fuel from tank
+                PartResourceList.Remove(Fuel1Name); //Removing fuel from tank
                 //Need help with this. Create a variable up top, or is inline ok?
                 Debug.Log("BHT Fuel1 amount set to 0.0, BH off");
                 if (FuelTypes == 2)
@@ -133,7 +133,7 @@ namespace BHTKSP
                     fuel2MaxAmount = GetMaxResourceAmount(Fuel2Name);
                     fuel2MaxAmount = fuel2LastAmount;
                     Debug.Log("BHT has gotten fuel2 values");
-                    public bool Remove(Fuel2Name); //Removing fuel from tank
+                    PartResourceList.Remove(Fuel2Name); //Removing fuel from tank
                     Debug.Log("BHT Fuel2 amount set to 0.0, BH off");
                 }
                 else
