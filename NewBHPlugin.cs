e586bec6112e015347273e924abd6828acd6585e
Benjamin Cronin <bcronin720@gmail.com>
2017-05-30 10:19:19 -0700

    Create NewBHPlugin.cs

---
+++ NewBHPlugin.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+
+namespace BlackHoleTank
+{
+    public class ModuleBlackHole : PartModule
+    {
+        [KSPField]
+        public string Fuel1Name;
+        
+        [KSPField]
+        public string Fuel2Name;
+        
+        [KSPField]
+        public double
